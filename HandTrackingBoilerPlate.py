1d4735c6c9c6f9cb10270bd18074408b6129e1b4
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-04-05 19:24:59 +0530

    Commented out extra code

--- HandTrackingBoilerPlate.py
+++ HandTrackingBoilerPlate.py
@@ -23,9 +23,9 @@ while True:
                 # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                print(id, cx, cy)
+                # print(id, cx, cy)
                 # if id == 4:
-                cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+                # cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
