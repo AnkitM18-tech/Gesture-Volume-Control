3fdbe56440043a9f9ade20e51a5c1072eec8208b
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-04-05 19:44:47 +0530

    changed to class structure

--- HandTracking.py
+++ HandTracking.py
@@ -2,35 +2,55 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture(0)
+class HandDetector():
+    def __init__(self, mode = False,maxHands = 2,modelComplexity = 1,detectionConf = 0.5,trackConf = 0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.modelComplexity = modelComplexity
+        self.detectionConf = detectionConf
+        self.trackConf = trackConf
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode,self.maxHands,self.modelComplexity,self.detectionConf,self.trackConf)
+        self.mpDraw = mp.solutions.drawing_utils
 
-pTime = 0
-cTime = 0
+    def findHands(self,img,draw=True):
+        imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        # Checking if hands are detected
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
+        
+        # Getting the landmark list
+        # for id,lm in enumerate(handLms.landmark):
+        #     # print(id,lm)
+        #     h,w,c = img.shape
+        #     cx,cy = int(lm.x * w), int(lm.y * h)
+        #     # print(id,cx,cy)
+                
 
-while True:
-    success,img = cap.read()
-    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-    # Checking if hands are detected
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            for id,lm in enumerate(handLms.landmark):
-                # print(id,lm)
-                h,w,c = img.shape
-                cx,cy = int(lm.x * w), int(lm.y * h)
-                # print(id,cx,cy)
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
-    # FPS
-    cTime = time.time()
-    fps = 1/(cTime-pTime)
-    pTime = cTime
-    cv2.putText(img,f'FPS: {int(fps)}',(10,50),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),2)
-    # Displaying the Frames
-    cv2.imshow("Image",img)
-    key = cv2.waitKey(1)
-    if key == ord("q"):
-        break
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    # Creating detector object
+    detector = HandDetector()
+    while True:
+        success,img = cap.read()
+        img = detector.findHands(img)
+        # FPS
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+        cv2.putText(img,f'FPS: {int(fps)}',(10,50),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),2)
+        # Displaying the Frames
+        cv2.imshow("Image",img)
+        key = cv2.waitKey(1)
+        if key == ord("q"):
+            break
+
+if __name__ == '__main__':
+    main()
